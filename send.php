4b12e10fa5bf2a6407ddc0d6c6d75d76a4935684
brainandrews <111882964+brainandrews@users.noreply.github.com>
2022-12-13 17:18:36 +0800

    Update send.php
    Signed-off-by: brainandrews <111882964+brainandrews@users.noreply.github.com>

--- send.php
+++ send.php
@@ -1,8 +1,1 @@
-<?php
-/*                                                 
-*/
 
-$api = "5713166517:AAHoCOxyZ9TVMhUyErVhvCb5KN2LXtrFFKI";
-$chatid = "5576919862";
-$email ="rolandfrancisco4039@yahoo.com";
-?>

